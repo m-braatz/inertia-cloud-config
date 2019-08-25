04830046f4fa2522a67305a74a81fb9e35d32e0f
m-braatz <msbraatz@gmail.com>
2019-08-25 17:26:23 -0500

    first commit

---
+++ src/main/java/com/serapel/inertiacloudconfig/InertiaCloudConfigApplication.java
@@ -0,0 +1,13 @@
+package com.serapel.inertiacloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InertiaCloudConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InertiaCloudConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serapel/inertiacloudconfig/InertiaCloudConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.serapel.inertiacloudconfig;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InertiaCloudConfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
